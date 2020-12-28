944ab279fb610b135a9092b19c866ea8688eca4b
bjzhang <bjzhang1991@gmail.com>
2020-12-28 16:39:56 +0800

    update os

--- os/main/os_example.go
+++ os/main/os_example.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"reflect"
 )
 
 func main() {
+	// 定义一个文件
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Get pwd path failed! error := %s", err.Error())
+	}
+	logpath := pwd + "/configs/os"
+	log.Printf("log path := %s", logpath)
+	_, err = os.Stat(logpath)
+	// 如果目录不存在则创建一个
+	if err != nil {
+		if !os.IsExist(err) {
+			os.MkdirAll(logpath, os.ModePerm)
+		}
+	}
+	// 生成文件数据
+	fileName := logpath + "/osexample.txt"
 	//创建文件，打开文件
-	file, _ := os.Create("osexample.txt")
-	file.WriteString("os.example.test")
+	file, _ := os.Create(fileName)
+	file.WriteString("os.example.test\n")
 	defer file.Close()
 	fmt.Println(reflect.ValueOf(file).Type())
 
@@ -19,7 +36,7 @@ func main() {
 	/***
 	hello /home/work/software/go，
 	$GOROOT替换为环境变量的值，而h没有环境变量可以替换，返回空字符串
-	 */
+	*/
 	fmt.Println(os.ExpandEnv("hello $GOROOT"))
 	fmt.Println(os.Getenv("GOPATH"))
 	fmt.Println(os.Getpid())
