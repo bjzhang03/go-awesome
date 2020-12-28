a349ac22c2c1c4bd8cd123d3b0a7cdb19b6cd51d
bjzhang <bjzhang1991@gmail.com>
2020-12-28 16:46:56 +0800

    update os example

--- os/main/openfile.go
+++ os/main/openfile.go
@@ -6,7 +6,21 @@ import (
 )
 
 func main() {
-	f, err := os.OpenFile("notes.txt", os.O_RDWR|os.O_CREATE, 0755)
+	// 定义一个文件
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Get pwd path failed! error := %s", err.Error())
+	}
+	logpath := pwd + "/configs/os"
+	log.Printf("log path := %s", logpath)
+	_, err = os.Stat(logpath)
+	// 如果目录不存在则创建一个
+	if err != nil {
+		if !os.IsExist(err) {
+			os.MkdirAll(logpath, os.ModePerm)
+		}
+	}
+	f, err := os.OpenFile(logpath+"/notes.txt", os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
